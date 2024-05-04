3b6a1c5298a94e5734410112affc8131e9ab0935
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-04 16:05:48 +0200

    feat: add more constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,15 @@
 export const BREAKPOINTS = {
   tablet: "992px",
   mobile: "468px"
-};
+} as const;
 
+export const CLUB_NAME = "Limoges TC Garden";
 export const CLUB_EMAIL = "contact@ltcg.eu";
 export const CLUB_PHONE = "05 55 34 24 08";
+export const CLUB_ID = "59 87 0006";
+
+export const CLUB_ADDRESS = {
+  STREET: "41 Rue Louis Casimir Ranson",
+  POSTAL_CODE: "87000",
+  CITY: "Limoges"
+} as const;
