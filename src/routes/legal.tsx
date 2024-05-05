75cfbe2fc0d2d1aa36f21ba24b418cda39c8179d
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-05 18:14:50 +0200

    fix: chars and justify

--- src/routes/legal.tsx
+++ src/routes/legal.tsx
@@ -6,7 +6,7 @@ const Page: Component = () => {
   return (
     <>
       <BrowserTitle title="Mention légales" />
-      <main class="max-w-[1000px] mx-auto px-6">
+      <main class="max-w-[1000px] mx-auto px-6 text-justify">
         <h1 class="font-bold text-3xl pb-6 text-center">
           Mention légales
         </h1>
@@ -18,7 +18,7 @@ const Page: Component = () => {
         <h3 class="font-medium text-lg py-4">1. Coordonnées personnelles</h3>
 
         <p>
-          Dans certains cas, le club peut être amené à demander aux internautes de laisser leurs coordonnées personnelles, notamment les adresses électroniques et/ou physiques. Ces informations pourront donner lieu à un traitement informatique aux fins de publipostage [envoi de messages promotionnels, d’information etc.] par le club, qui pourra également être amené à les échanger ou les céder à ses partenaires commerciaux. Chacune de ces demandes, consultables le cas échéant aux pages concernées du site Internet du club, est accompagnée d'une case à cocher, autorisant ou non le club à utiliser les données collectées. En cas d'acceptation par la validation de la case correspondant à l’utilisation souhaitée, le club se réserve le droit d'adresser des publipostages à l’utilisateur pour lui proposer des informations, des offres émanant du club ou de ses partenaires commerciaux, et/ou de procéder à l’échange ou à la cession, à titre gratuit ou onéreux, des informations considérées.
+          Dans certains cas, le club peut être amené à demander aux internautes de laisser leurs coordonnées personnelles, notamment les adresses électroniques et/ou physiques. Ces informations pourront donner lieu à un traitement informatique aux fins de publipostage [envoi de messages promotionnels, d'information etc.] par le club, qui pourra également être amené à les échanger ou les céder à ses partenaires commerciaux. Chacune de ces demandes, consultables le cas échéant aux pages concernées du site Internet du club, est accompagnée d'une case à cocher, autorisant ou non le club à utiliser les données collectées. En cas d'acceptation par la validation de la case correspondant à l'utilisation souhaitée, le club se réserve le droit d'adresser des publipostages à l'utilisateur pour lui proposer des informations, des offres émanant du club ou de ses partenaires commerciaux, et/ou de procéder à l'échange ou à la cession, à titre gratuit ou onéreux, des informations considérées.
         </p>
 
         <h3 class="font-medium text-lg py-4">2. Droits</h3>
@@ -34,7 +34,7 @@ const Page: Component = () => {
         </div>
 
         <p>
-        L’utilisateur peut également, pour des motifs légitimes, s’opposer au traitement des données le concernant. L'utilisateur trouvera des informations sur ses droits et devoirs et sur la protection des données individuelles sur le site de la Commission Nationale Informatique et Libertés (www.cnil.fr). Les utilisateurs de ce site Internet sont invités à faire connaître au club leurs remarques sur d'éventuels dysfonctionnement du site au regard des libertés individuelles, à l'adresse indiquée email ci-avant.
+          L'utilisateur peut également, pour des motifs légitimes, s'opposer au traitement des données le concernant. L'utilisateur trouvera des informations sur ses droits et devoirs et sur la protection des données individuelles sur le site de la Commission Nationale Informatique et Libertés (www.cnil.fr). Les utilisateurs de ce site Internet sont invités à faire connaître au club leurs remarques sur d'éventuels dysfonctionnement du site au regard des libertés individuelles, à l'adresse indiquée email ci-avant.
         </p>
       </main>
     </>
